ac82a5679daa91e21aa57770751950487cbdbc40
Matthew Berry <berrymatthewj@gmail.com>
2021-04-22 13:43:30 -0600

    fix connection string

--- static/js/app.js
+++ static/js/app.js
@@ -3,61 +3,61 @@
 //===============================
 
 // Create URL to the Flask app route
-const url = "/loadData";
+const url = process.env.MONGODB_URI;
 
 //-------------------------
 //  Dropdown menu
 //-------------------------
 
 // Fetch the JSON data and console log it
-function dropDownMenu() {    
-  
+function dropDownMenu() {
+
     // use d3 to select the dropdown html element
     var dropdownYear = d3.select("#selYear");
     var dropdownState = d3.select("#selState");
-  
-    // Load data from  file and assign to variable
-    d3.json(url).then(function(data) {  
-      console.log(data);     
-  
-      // create list of unique values of years
-      var allYears = d3.map(data, function(d) {return (d.year)}).keys();
-      //console.log(allYears);      
-  
-      // create list of unique values of states
-      var allStates = d3.map(data, function(d) { return (d.jurisdiction)}).keys();
-      //console.log(allStates);
-      
-      // create dropdown menu for Years
-      for (var i=0; i < allYears.length; i++) {
-        dropdownYear.append("option").text(allYears[i]).property("value", allYears[i]);
-      }
-  
-      // create dropdown menu for States 
-      for (var i=0; i < allStates.length; i++) {
-        dropdownState.append("option").text(allStates[i]).property("value", allStates[i].jurisdiction);
-      }
-  
-  
-      // initialize the dashboard with the first state and year
-      var theFirst = data[0];      
-  
-      var initialState = theFirst.jurisdiction;      
-      var initialYear = theFirst.year;      
-  
-      stateSummaryInfo(initialYear, initialState);
-      buildPieChart(initialYear, initialState);
-      buildLinePlot(initialState);
-      buildMap(initialYear);
 
-    });    
+    // Load data from  file and assign to variable
+    d3.json(url).then(function(data) {
+        console.log(data);
+
+        // create list of unique values of years
+        var allYears = d3.map(data, function(d) { return (d.year) }).keys();
+        //console.log(allYears);      
+
+        // create list of unique values of states
+        var allStates = d3.map(data, function(d) { return (d.jurisdiction) }).keys();
+        //console.log(allStates);
+
+        // create dropdown menu for Years
+        for (var i = 0; i < allYears.length; i++) {
+            dropdownYear.append("option").text(allYears[i]).property("value", allYears[i]);
+        }
+
+        // create dropdown menu for States 
+        for (var i = 0; i < allStates.length; i++) {
+            dropdownState.append("option").text(allStates[i]).property("value", allStates[i].jurisdiction);
+        }
+
+
+        // initialize the dashboard with the first state and year
+        var theFirst = data[0];
+
+        var initialState = theFirst.jurisdiction;
+        var initialYear = theFirst.year;
+
+        stateSummaryInfo(initialYear, initialState);
+        buildPieChart(initialYear, initialState);
+        buildLinePlot(initialState);
+        buildMap(initialYear);
+
+    });
 }
 
 
 //-------------------------
 //  Summary Info Panel
 //-------------------------
-function stateSummaryInfo(chosenYear, chosenState) {    
+function stateSummaryInfo(chosenYear, chosenState) {
 
     d3.json(url).then((data) => {
 
@@ -109,11 +109,11 @@ function buildPieChart(chosenYear, chosenState) {
             }
         });
         console.log(dataArray);
-        
+
         var values = [];
         var labels = [];
 
-        Object.entries(dataArray[0]).forEach(([key, value]) => {                   
+        Object.entries(dataArray[0]).forEach(([key, value]) => {
             values.push(value);
             labels.push(key);
         });
@@ -124,23 +124,23 @@ function buildPieChart(chosenYear, chosenState) {
         var pieChartData = [{
             values: values,
             labels: labels,
-            type: 'pie',            
+            type: 'pie',
             text: 'violent_crime_total',
             textposition: 'inside',
-            domain: { column: 0 },            
+            domain: { column: 0 },
             hole: .5
         }];
 
         var layout = {
             height: 400,
             width: 500,
-            title: '<b>'+`${chosenState}`+", "+`${chosenYear}`+'</b><br>Types of Crime',
+            title: '<b>' + `${chosenState}` + ", " + `${chosenYear}` + '</b><br>Types of Crime',
             annotations: [{
                 text: "<b>Total: </b><br>violent crime<br>" + `${filteredData[0].violent_crime_total}` + "<br>property crime<br>" + `${filteredData[0].property_crime_total}`,
                 x: 0.55,
                 y: 0.35
             }]
-        };        
+        };
 
         Plotly.newPlot("pie", pieChartData, layout);
 
@@ -194,7 +194,7 @@ function buildMap(chosenYear) {
     d3.json(url).then((data) => {
         var filteredData = data.filter(d => d.year === chosenYear)
         console.log(filteredData);
-        
+
         var dataArray = filteredData.map(function(d) {
             return {
                 jurisdiction: d.jurisdiction,
@@ -203,13 +203,13 @@ function buildMap(chosenYear) {
                 prisoner_count: +d.prisoner_count
             }
         });
-        console.log(dataArray);        
+        console.log(dataArray);
 
         var mapData = [{
             type: 'choropleth',
             locationmode: 'USA-states',
-            locations: unpack(dataArray, 'location'),            
-            z: unpack(dataArray, 'prisoner_count'), 
+            locations: unpack(dataArray, 'location'),
+            z: unpack(dataArray, 'prisoner_count'),
             text: unpack(dataArray, "jurisdiction"),
             colorbar: {
                 title: '<b># of Prisoners</b>'
