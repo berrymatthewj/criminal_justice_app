57075e5ac5709dfdb33c12a488c14effc061c84d
agkden <agkden@yahoo.com>
2021-04-22 22:02:40 -0600

    files updated

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 import pymongo
 from bson.json_util import dumps
+import os
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
 # Create connection variable
-conn = 'mongodb://localhost:27017'
+conn = os.environ.get('MONGODB_URI')
 
 # Pass connection to the pymongo instance.
 client = pymongo.MongoClient(conn)
@@ -17,8 +18,9 @@ db = client.project_2
 # Set route
 @app.route('/')
 def index():
-    query = list(db.incarceration.find())
-    return render_template("index.html", query=query)
+    # query = list(db.incarceration.find())
+    return render_template("index.html")
+    #query=query
     
 
 @app.route('/loadData')
